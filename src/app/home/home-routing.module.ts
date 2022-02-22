5dd8f2d00a73d8cf0f853c399682b7e3a1bb15bc
Manuel Gonzalez Blanco <manuel.gonzalez@dogtorapp.com>
2022-02-22 13:32:14 +0100

    primera subida palabricks

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SettingsComponent } from '../settings/settings.component';
 import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
   }
 ];
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
